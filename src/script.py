a89138696216c502dcb8e823e7a46c6df25613b4
erzaosmani <erzaosmanii578@gmail.com>
2025-03-12 00:40:56 +0100

    Load the new dataset

--- src/script.py
+++ src/script.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-csv_data = pd.read_csv("synthetic_fraud_dataset.csv")
+csv_data = pd.read_csv("atk-investimet-tvsh.csv")
 
 # Printing the data types
 print("Data types for each field:")
@@ -20,12 +20,10 @@ def adv_miss_check(df):
         df = df.map(lambda x: str(x).lower() if isinstance(x, str) else x)
         missing_rows = df[df.apply(lambda row: row.isin(no_info_types).any(), axis=1)]
         return missing_rows
-
 adv_miss_check(csv_data)
 
 # Check for duplicate
 duplicate_values = csv_data.duplicated().sum()
-
 print(f"Number of duplicate rows: {duplicate_values}")
 
 # Check value counts
@@ -34,4 +32,3 @@ for column in categorical_columns:
     print(f"Value counts for {column}:")
     print(csv_data[column].value_counts())
     print()
-
