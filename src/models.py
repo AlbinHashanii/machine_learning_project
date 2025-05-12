f5d4c5bbec50081fa9c4209f8ed07411a67e3842
erzaosmani <erzaosmanii578@gmail.com>
2025-05-13 00:48:38 +0200

    Fix the classes for XGB, CatBoost and LGBM

--- src/models.py
+++ src/models.py
@@ -2,9 +2,9 @@ from sklearn.cluster import KMeans
 from sklearn.linear_model import LinearRegression
 from sklearn.naive_bayes import GaussianNB
 from sklearn.ensemble import RandomForestClassifier
-from lightgbm import LGBMRegressor
-from xgboost import XGBRegressor
-from catboost import CatBoostRegressor
+from lightgbm import LGBMClassifier
+from xgboost import XGBClassifier
+from catboost import CatBoostClassifier
 from tensorflow.keras import layers, models
 
 def build_autoencoder(X_train):
@@ -33,16 +33,13 @@ def train_linear_regression(X, y):
     return model
 
 def train_lgbm(X, y):
-    model = LGBMRegressor(random_state=42).fit(X, y)
-    return model
+    return LGBMClassifier(random_state=42).fit(X, y)
 
 def train_xgboost(X, y):
-    model = XGBRegressor(random_state=42).fit(X, y)
-    return model
+    return XGBClassifier(random_state=42, use_label_encoder=False, eval_metric='mlogloss').fit(X, y)
 
 def train_catboost(X, y):
-    model = CatBoostRegressor(random_state=42, verbose=0).fit(X, y)
-    return model
+    return CatBoostClassifier(random_state=42, verbose=0).fit(X, y)
 
 def train_random_forest(X, y):
     model = RandomForestClassifier(random_state=42, class_weight='balanced', n_estimators=100).fit(X, y)
